05d4fe7e16d53de22f2916ea328773371a638d65
Rafael <raphasalimov@gmail.com>
2024-05-26 15:04:45 +0400

    feat: Implemented tickets service for teens

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,5 +1,5 @@
 from pytemplate.domain.models import Movie
-from pytemplate.service.tickets import buy_ticket_for_children
+from pytemplate.service.tickets import buy_ticket_for_children, buy_ticket_for_teens
 
 
 def test_buy_ticket_for_children_under_age():
@@ -17,4 +17,22 @@ def test_buy_ticket_for_children_appropriate_age():
 def test_buy_ticket_for_children_over_age():
     movie = Movie(name="Elemental", customer_age=10)
     result = buy_ticket_for_children(movie)
+    assert result == "You are allowed to watch Elemental!"
+
+
+def test_buy_ticket_for_teens_under_age():
+    movie = Movie(name="Elemental", customer_age=12)
+    result = buy_ticket_for_teens(movie)
+    assert result == "Sorry, you are not old enough to watch Elemental!"
+
+
+def test_buy_ticket_for_teens_appropriate_age():
+    movie = Movie(name="Elemental", customer_age=15)
+    result = buy_ticket_for_teens(movie)
+    assert result == "You are allowed to watch Elemental!"
+
+
+def test_buy_ticket_for_teens_over_age():
+    movie = Movie(name="Elemental", customer_age=13)
+    result = buy_ticket_for_teens(movie)
     assert result == "You are allowed to watch Elemental!"
